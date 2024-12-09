475e692b9b6f5f6a51e9cd944d3a5c076ef50f4d
KonarAndriiana <andriiana.konar@kosickaakademia.sk>
2024-12-09 09:28:53 +0100

    Rename project from 'book-manager' to 'BookManagaer' and update references accordingly

--- BookManager/README.md
+++ BookManager/README.md
@@ -1,4 +1,4 @@
-# BookManager
+# BookManagaer
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.0.4.
 
--- BookManager/src/app/app.component.spec.ts
+++ BookManager/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'book-manager' title`, () => {
+  it(`should have the 'BookManagaer' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('book-manager');
+    expect(app.title).toEqual('BookManagaer');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, book-manager');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, BookManagaer');
   });
 });
