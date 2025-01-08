8b3239755670fccffff8f34b66c038e2540372fd
KonarAndriiana <andriiana.konar@kosickaakademia.sk>
2025-01-08 02:18:26 +0100

    Remove BookRepository and StatusRepository interfaces as they are no longer needed

--- BookManager/src/main/java/sk/kasv/DAO/StatusRepository.java
+++
@@ -1,7 +0,0 @@
-package sk.kasv.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sk.kasv.entity.Status;
-
-public interface StatusRepository extends JpaRepository<Status, Integer> {
-}
