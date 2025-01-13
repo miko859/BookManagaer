f59bf424b45c5f81472b517591c462bbb5dc0c85
KonarAndriiana <andriiana.konar@kosickaakademia.sk>
2025-01-13 19:27:48 +0100

    Remove unused login and dashboard components

--- book-manager/src/app/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-
-export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
-];
